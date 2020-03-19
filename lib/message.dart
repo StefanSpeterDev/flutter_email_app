640fdde46761238e563fec2b304ce1ffedc21130
StefanYYC <stefanspeter26@gmail.com>
2020-03-19 15:51:09 +0100

    add our message.json to an online serv to get it through http

--- lib/message.dart
+++ lib/message.dart
@@ -10,6 +10,10 @@ class Message{
   Message(this.subject, this.body);
 
   // Constructor that tells dart that when he receives a map, here is how to translate it into an instance of message
+  // Now use build_runner and json_serializable to automatically update the dart object with json's good values,
+  // so we don't have to do it (like subject, body...)
+  // We can check this file in the generated file message.g.dart (generated using :
+  // flutter packages pub run build_runner build )
   factory Message.fromJson(Map<String, dynamic> json)
       => _$MessageFromJson(json);
 
